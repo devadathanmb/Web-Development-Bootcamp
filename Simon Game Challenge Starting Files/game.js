ccb1659c5abd5576fb1d9043608f313604ddad59
Devadathan M B <devadathanmb@gmail.com>
2022-01-21 22:12:49 +0530

    Completed Simon Game Challenge

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,7 +2,9 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedpattern = [];
 var level = 0;
-function nextSequence(){
+
+//Function to generate the sequence
+function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColours[randomNumber];
   gamePattern.push(randomChosenColor);
@@ -12,48 +14,68 @@ function nextSequence(){
   $("#level-title").text("Level " + level);
 }
 
-var numberOfClicks = -1;
-$(".btn").click(function(event){
-  numberOfClicks++;
+
+//Index variable keeps track of the user's position in the sequence
+var index = 0;
+
+// On clicking the buttons
+$(".btn").click(function(event) {
   var userChosenColor = event.target.id;
   userClickedpattern.push(userChosenColor);
   playSound(userChosenColor);
   animatePress(userChosenColor);
-  checkUserClicks(userChosenColor);
-  // console.log(userClickedpattern);
+  checkAnswer(userChosenColor);
 });
 
-function playSound(name){
+
+//Function to play sound
+function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-function animatePress(currentColor){
+
+//Function to animate click
+function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-  setTimeout(function(){
+  setTimeout(function() {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
 
+
+//Start the game when any key is pressed
 var hasGameStarted = false;
-$(document).keypress(function(){
-  if(hasGameStarted === true){
+$(document).keypress(function() {
+  if (hasGameStarted === true) {
     return;
   }
   nextSequence();
   hasGameStarted = true;
 });
 
-function checkUserClicks(color){
-  if(gamePattern[numberOfClicks] != userClickedpattern[numberOfClicks]){
+
+//Function to checck if the user sequence matches the random sequence
+function checkAnswer(color) {
+  if (gamePattern[index] != color) {
     playSound("wrong");
     $("body").addClass("game-over");
-    setTimeout(function(){
+    setTimeout(function() {
       $("body").removeClass("game-over");
     }, 200);
-    #("#level-title").text("Game Over, Press Any Key to Restart");
-    numberOfClicks = -1;
+    $("#level-title").text("Game Over, Press Any Key to Restart");
     userClickedpattern = [];
     gamePattern = [];
+    index = 0;
+    level = 0;
+    hasGameStarted = false;
+  } else if (gamePattern[index] == color) {
+    index++;
+    if (index == gamePattern.length) {
+      index = 0;
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
   }
 }
