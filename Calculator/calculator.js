447597b8332df6da0d8bbc0c8ab52fbec013939e
Devadathan M B <devadathanmb@gmail.com>
2022-01-22 12:46:41 +0530

    Completed Calculator Web App

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,20 @@
 //jshint esversion:6
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var ans = num1 + num2;
+  // res.send("The answer is " + ans);
+  res.send(ans.toString());
 });
 
 app.listen("3000");
