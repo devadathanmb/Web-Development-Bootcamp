c1dcc8ede8ae832b9fe1f14bdb8dc770e7eece11
Devadathan M B <devadathanmb@gmail.com>
2022-02-01 20:24:26 +0530

    ..

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -3,46 +3,149 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
-app.get("/", function(req, res) {
+//Creating a new DB
+mongoose.connect("mongodb+srv://admin-me:HJfhdJLZ8808ypIk@cluster0.j354x.mongodb.net/todolistDB");
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+//Creating a schema for the collection
+const itemSchema = new mongoose.Schema({
+  name: String
 });
 
-app.post("/", function(req, res){
+//Creating a model of that Schema
+const Item = mongoose.model("Item", itemSchema);
 
-  const item = req.body.newItem;
+//Creating a document formt that model
+const item1 = new Item({
+  name: "Welcome to the to do list"
+});
+const item2 = new Item({
+  name: "Hit + to add a new item"
+});
+const item3 = new Item({
+  name: "<--- Hit here to delete an item"
+});
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function(req, res) {
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log("Error", err);
+        } else {
+          console.log("Successfully added items to the DB");
+        }
+      });
+    }
+    res.render("list", {
+      listTitle: "Today",
+      newListItems: foundItems
+    });
+  });
+});
+
+app.post("/", function(req, res) {
+  const listName = req.body.list;
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
+  if (listName == "Today") {
+    item.save();
     res.redirect("/");
+  } else {
+    List.findOne({
+      name: listName
+    }, (err, foundList) => {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+  if (listName === "Today") {
+    Item.deleteOne({
+      _id: checkedItemId
+    }, (err) => {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Successfully deleted item from DB");
+      }
+    });
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: checkedItemId
+        }
+      }
+    }, (err, foundList) => {
+      if (!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
 });
 
-app.get("/about", function(req, res){
+app.get("/:category", (req, res) => {
+  const customListName = _.capitalize((req.params.category));
+  List.findOne({
+      name: customListName
+    },
+    (err, foundList) => {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render("list", {
+          listTitle: customListName,
+          newListItems: foundList.items
+        });
+      }
+    })
+});
+
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+//HJfhdJLZ8808ypIk
+//admin-me
