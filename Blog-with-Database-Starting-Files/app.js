95e15c209d13ccb8286fac5361cd3233b39dc93f
Devadathan M B <devadathanmb@gmail.com>
2022-02-02 15:13:22 +0530

    Completed Blog Website Challenge

--- Blog-with-Database-Starting-Files/app.js
+++ Blog-with-Database-Starting-Files/app.js
@@ -16,12 +16,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const homeStartingContent = new Blog({
-  content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
-});
-
-
-
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -35,22 +30,17 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-let posts = [];
 
 app.get("/", function(req, res) {
   Blog.find({}, (err, foundBlogs) => {
     if (!err) {
-      if (foundBlogs.length === 0) {
-        homeStartingContent.save();
-        console.log("Successfully logged into DB");
-      }
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: foundBlogs
+      });
     } else {
       console.log("Error", err);
     }
-  });
-  res.render("home", {
-    startingContent: homeStartingContent.content,
-    posts: posts
   });
 });
 
@@ -76,25 +66,23 @@ app.post("/compose", function(req, res) {
     content: req.body.postBody
   };
   const newPost = new Blog(post);
-  newPost.save();
-  posts.push(post);
-  res.redirect("/");
+  newPost.save(() => res.redirect("/"));
 });
 
-app.get("/posts/:postName", function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
+app.get("/posts/:postID", function(req, res) {
+  const requestedBlogId = req.params.postID;
+  Blog.findOne({
+    _id: requestedBlogId
+  }, (err, blog) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
       res.render("post", {
-        title: post.title,
-        content: post.content
+        title: blog.title,
+        content: blog.content
       });
     }
   });
-
 });
 
 app.listen(3000, function() {
